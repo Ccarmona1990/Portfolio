62be523822546664fb765c82b4223571bdc2181f
Christopher Carmona <c.carmona0990@gmail.com>
2021-12-05 10:32:06 -0400

    update
    headings update

--- src/utils/main/Projects.js
+++ src/utils/main/Projects.js
@@ -6,7 +6,7 @@ const Projects = () => {
     
     return (
         <section id='Projects' >
-            <h1>My Projects</h1>
+            <h1>Projects I've built</h1>
             <div className='projects'>
                 {projects.map(({id,name,url,img,description,tools})=>{
                     return (
